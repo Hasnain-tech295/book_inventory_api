f4b7aba46566a51abb159cc75009d441aeacaa09
Hasnain-tech295 <hasnainansari2430@gmail.com>
2026-04-18 11:52:21 +0530

    chore: remove unused dependencies from requirements.txt

--- day_06/database.py
+++
@@ -1,9 +0,0 @@
-# Everything SQLAlchemy needs to connect and create sessions.
-# This file is the bridge between your app and PostgreSQL.
-
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
-from config import get_settings
-
-settings = get_settings()
-
